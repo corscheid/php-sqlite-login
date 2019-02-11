201a897b607b1f594bb4bae6a1068b5590c88946
Tirea Aean <tirea@learnnavi.org>
2019-02-11 02:39:55 -0500

    Initial commit

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+
+function echo_form() {
+  echo '';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login</title>
+</head>
+<body>
+  <form action="login.php" method="post"></form>
+</body>
+</html>
